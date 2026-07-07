9f3ed57225e7cb72dfb5bc379ce58799d872212a
Srinavin Nair <zencoder1985@gmail.com>
2026-07-06 19:08:48 -0700

    Some more config for github pages failing

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  base: './',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/Classics_MsPacman/' : '/',
   server: {
     host: '0.0.0.0',
     port: 5173,
   },
-});
+}));
