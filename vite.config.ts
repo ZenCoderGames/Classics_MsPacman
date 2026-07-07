73b5516dd860d601ec51ba0ea64161aab08b6163
Srinavin Nair <zencoder1985@gmail.com>
2026-07-06 19:19:53 -0700

    More commits to fix github page link

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 
+const repoBase = '/Classics_MsPacman/';
+
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/Classics_MsPacman/' : '/',
+  base: mode === 'production' ? repoBase : '/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
